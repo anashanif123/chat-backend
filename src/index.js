138b1fa39297e9142ef2ec5d61e69aaec6dac589
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 19:48:39 +0500

    deployement issue

--- src/index.js
+++ src/index.js
@@ -1,43 +1,33 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js"
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
-import messageRoutes from "./routes/message.routes.js"
-import cors from 'cors'
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config()
-// connecting database 
-connectDB()
+dotenv.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-// middlewares 
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin:true,
-    credentials:true // send cookies etc 
-}))
-app.use(cookieParser())
-// app.use(express.json())
-app.use(express.json({ limit: '10mb' }))
+    credentials: true,
+  })
+);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-
-app.get("/",(req,res)=>{
-    res.send("working fine")
-})
-
-
-
-// apis on app 
-app.use("/api/auth" , authRoutes)
-//messages api
-app.use("/api/message" ,messageRoutes)
-
-server.listen(PORT ,()=>{
-console.log("app is running on PORT:" ,PORT);
-
-
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
