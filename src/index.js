a1504523ed163bb56444191eeda921e69a6ddaaa
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-06-08 19:40:54 +0500

    deployement issue

--- src/index.js
+++ src/index.js
@@ -1,39 +1,43 @@
 import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-
-import path from "path";
-
+import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
-
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
+import cookieParser from "cookie-parser"
+import messageRoutes from "./routes/message.routes.js"
+import cors from 'cors'
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
+dotenv.config()
+// connecting database 
+connectDB()
 
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const PORT = process.env.PORT
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://chat-frontend-sigma-rouge.vercel.app",
-      "https://chat-frontend-murex.vercel.app",
-      "https://chat-frontend-jpbkyyrkj-anas-projects-dfbef841.vercel.app",
-    ],
-    credentials: true,
-  })
-);
+// middlewares 
+app.use(cors({
+    origin:true,
+    credentials:true // send cookies etc 
+}))
+app.use(cookieParser())
+// app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+
+
+app.get("/",(req,res)=>{
+    res.send("working fine")
+})
+
+
+
+// apis on app 
+app.use("/api/auth" , authRoutes)
+//messages api
+app.use("/api/message" ,messageRoutes)
+
+server.listen(PORT ,()=>{
+console.log("app is running on PORT:" ,PORT);
+
+
+})
